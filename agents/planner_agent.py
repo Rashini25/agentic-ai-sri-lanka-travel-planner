698243ebac1bfab38f400db6ebf1a8d37e5d6327
Rashini25 <rashiniprabodha2002@gmail.com>
2026-07-26 18:05:23 +0530

    feat: add preference agent and integrate with planner

--- agents/planner_agent.py
+++ agents/planner_agent.py
@@ -1,3 +1,4 @@
+from agents.preference_agent import PreferenceAgent
 from agents.rag_agent import rag_agent
 from agents.transport_agent import TransportAgent
 from agents.accommodation_agent import AccommodationAgent
@@ -7,41 +8,56 @@ from agents.itinerary_agent import ItineraryAgent
 transport_agent = TransportAgent()
 accommodation_agent = AccommodationAgent()
 itinerary_agent = ItineraryAgent()
+preference_agent = PreferenceAgent()
 
 
-def planner_agent(destination, budget):
+def planner_agent(destination, budget, preferences):
 
     # Get travel information from RAG Agent
     travel_info = rag_agent(destination)
 
+
     # Structured message for other agents
     planner_message = {
         "sender": "Planner Agent",
+
         "travel_details": {
             "destination": destination,
             "budget": budget
         }
     }
 
+
+    # Analyze user preferences
+    preference_info = preference_agent.process_request(
+        preferences
+    )
+
+
     # Get transport recommendations
     transport_info = transport_agent.process_request(
         planner_message
     )
+
 
     # Get accommodation recommendations
     accommodation_info = accommodation_agent.process_request(
         planner_message
     )
 
+
     # Get itinerary recommendations
     itinerary_info = itinerary_agent.process_request(
         planner_message
     )
 
+
     return {
         "sender": "Planner Agent",
 
         "destination": destination,
+
+        "preference_information": preference_info,
 
         "travel_information": travel_info,
 
@@ -53,11 +69,20 @@ def planner_agent(destination, budget):
     }
 
 
+
 if __name__ == "__main__":
 
     result = planner_agent(
         "Kitulgala",
-        5000
+        5000,
+        {
+            "budget_type": "Low",
+            "travel_style": "Adventure",
+            "interests": [
+                "Adventure",
+                "Nature"
+            ]
+        }
     )
 
     print(result)
